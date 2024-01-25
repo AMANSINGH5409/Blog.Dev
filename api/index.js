bd48fbf947270adbcebaefd524cfae637baa1b1f
AMAN RAJAN SINGH <madhusingh74055@gmail.com>
2024-01-26 01:42:41 +0530

    Create SignUp API

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 dotenv.config();
+
+//app middlewares
+app.use(express.json());
+
+app.use("/api/user/", userRoutes);
+app.use("/api/auth/", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL)
