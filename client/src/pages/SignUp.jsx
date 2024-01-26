21970b77290554c0b7dd3c2e411e06759c13152c
AMAN RAJAN SINGH <madhusingh74055@gmail.com>
2024-01-26 15:29:21 +0530

    Added Functionality to SignUp Page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,55 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value.trim(),
+    });
+  };
+
+  console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setLoading(false);
+      setErrorMessage(error.message);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex justify-center p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -19,22 +66,48 @@ const SignUp = () => {
           </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <div className="">
               <Label value="Your Username" />
-              <TextInput type="text" placeholder="coolXyz50" id="username" />
+              <TextInput
+                onChange={handleChange}
+                type="text"
+                placeholder="coolXyz50"
+                id="username"
+              />
             </div>
             <div className="">
               <Label value="Your Email" />
-              <TextInput type="email" placeholder="xyz@gmail.com" id="email" />
+              <TextInput
+                onChange={handleChange}
+                type="email"
+                placeholder="xyz@gmail.com"
+                id="email"
+              />
             </div>
             <div className="">
               <Label value="Your Password" />
-              <TextInput type="password" placeholder="***234" id="password" />
+              <TextInput
+                onChange={handleChange}
+                type="password"
+                placeholder="***234"
+                id="password"
+              />
             </div>
 
-            <Button type="submit" gradientDuoTone={"purpleToPink"}>
-              Sign Up
+            <Button
+              type="submit"
+              gradientDuoTone={"purpleToPink"}
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size={"sm"} />
+                  <span>Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -43,6 +116,11 @@ const SignUp = () => {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color={"failure"}>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
